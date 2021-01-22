24501c2e4b2502df2ed15d3a27e24960e353329f
Marios Sirtmatsis <marios.sirtmatsis@hotmail.com>
2021-01-22 07:46:58 +0100

    Minor changes for better usability

--- main.py
+++ main.py
@@ -26,16 +26,15 @@ SAVE_MODEL_STEPS = 1000000
 SAVE_MODEL_PATH = "models_save/"
 LEARNING_RATE = 0.00025
 INPUT_DIMS = (84, 84, 4,)
-ENV_ID = "Breakout-v0"
 
 parser = argparse.ArgumentParser(description="dqn and dueling dqn")
-parser.add_argument("--dueling", type=int)
-parser.add_argument("--double", type=int)
-parser.add_argument("--test", type=int)
+parser.add_argument("--dueling", type=int, default=0, help="Dueling DQN => 0 = No, 1 = Yes")
+parser.add_argument("--double", type=int, default=0, help="DDQN => 0 = No, 1 = Yes")
+parser.add_argument("--env", type=str, default= "Breakout-v0", help="Type in the env name")
 args = parser.parse_args()
 
 if __name__ == "__main__":
-    env = make_atari(ENV_ID)
+    env = make_atari(args.env)
     env = wrap_deepmind(env, episode_life= True, clip_rewards= True, frame_stack= True, scale= True)
 
 
@@ -49,32 +48,26 @@ if __name__ == "__main__":
     model_dueling = Models.dueling_dqn(INPUT_DIMS, num_actions)
     model_target_dueling = Models.dueling_dqn(INPUT_DIMS, num_actions)
 
-    model_test = Models.load_model("models_save/double/model-08-01-202117-57-57.h5")
 
 
-    if args.test == 1:
-        agent = Agent(env, model_test, model_test, EPSILON_MAX)
-        agent.test(MAX_TEST_EPISODES, MAX_STEPS_P_EPISODE)
+    if args.dueling ==1:
+        print("DUELING")
+        agent = Agent(env, model_dueling, model_target_dueling, EPSILON_MAX)
+        agent.train(MAX_MEMORY_LENGTH, BATCH_SIZE, GAMMA, LEARNING_RATE,
+                    MAX_STEPS_P_EPISODE, MAX_EPISODES, MAX_FRAMES,  EPS_RANDOM_FRAMES,
+                    EPS_GREEDY_FRAMES, UPDATE_AFTER_ACTIONS, UPDATE_TARGET_NETWORK,
+                    SAVE_MODEL_STEPS, SAVE_MODEL_PATH, False)
+
+    elif args.double ==1:
+        print("DOUBLE")
+        agent = Agent(env, model,model_target, EPSILON_MAX)
+        agent.train(MAX_MEMORY_LENGTH, BATCH_SIZE, GAMMA, LEARNING_RATE,
+                    MAX_STEPS_P_EPISODE, MAX_EPISODES, MAX_FRAMES,  EPS_RANDOM_FRAMES,
+                    EPS_GREEDY_FRAMES, UPDATE_AFTER_ACTIONS, UPDATE_TARGET_NETWORK,
+                    SAVE_MODEL_STEPS, SAVE_MODEL_PATH, True)
     else:
-
-        if args.dueling ==1:
-            print("DUELING")
-            agent = Agent(env, model_dueling, model_target_dueling, EPSILON_MAX)
-            agent.train(MAX_MEMORY_LENGTH, BATCH_SIZE, GAMMA, LEARNING_RATE,
-                        MAX_STEPS_P_EPISODE, MAX_EPISODES, MAX_FRAMES,  EPS_RANDOM_FRAMES,
-                        EPS_GREEDY_FRAMES, UPDATE_AFTER_ACTIONS, UPDATE_TARGET_NETWORK,
-                        SAVE_MODEL_STEPS, SAVE_MODEL_PATH, True)
-
-        elif args.double ==1:
-            print("DOUBLE")
-            agent = Agent(env, model,model_target, EPSILON_MAX)
-            agent.train(MAX_MEMORY_LENGTH, BATCH_SIZE, GAMMA, LEARNING_RATE,
-                        MAX_STEPS_P_EPISODE, MAX_EPISODES, MAX_FRAMES,  EPS_RANDOM_FRAMES,
-                        EPS_GREEDY_FRAMES, UPDATE_AFTER_ACTIONS, UPDATE_TARGET_NETWORK,
-                        SAVE_MODEL_STEPS, SAVE_MODEL_PATH, True)
-        else:
-            agent = Agent(env, model,model_target, EPSILON_MAX)
-            agent.train(MAX_MEMORY_LENGTH, BATCH_SIZE, GAMMA, LEARNING_RATE,
-                        MAX_STEPS_P_EPISODE, MAX_EPISODES, MAX_FRAMES,  EPS_RANDOM_FRAMES,
-                        EPS_GREEDY_FRAMES, UPDATE_AFTER_ACTIONS, UPDATE_TARGET_NETWORK,
-                        SAVE_MODEL_STEPS, SAVE_MODEL_PATH, False)
+        agent = Agent(env, model,model_target, EPSILON_MAX)
+        agent.train(MAX_MEMORY_LENGTH, BATCH_SIZE, GAMMA, LEARNING_RATE,
+                    MAX_STEPS_P_EPISODE, MAX_EPISODES, MAX_FRAMES,  EPS_RANDOM_FRAMES,
+                    EPS_GREEDY_FRAMES, UPDATE_AFTER_ACTIONS, UPDATE_TARGET_NETWORK,
+                    SAVE_MODEL_STEPS, SAVE_MODEL_PATH, False)

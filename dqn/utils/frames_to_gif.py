ff4d01e04274ce588d097bd9588c7a45c6635b1d
Marios Sirtmatsis <marios.sirtmatsis@hotmail.com>
2021-02-16 19:17:45 +0100

    new demo

--- dqn/utils/frames_to_gif.py
+++ dqn/utils/frames_to_gif.py
@@ -4,7 +4,7 @@ import os
 images = []
 i = 0
 for filename in os.listdir('./frames'):
-    print(filename)
+    # print(filename)
     if i >= 700:
         break
     images.append(imageio.imread('./frames/frame{}.png'.format(i+300)))
